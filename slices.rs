0fd2c0c219ea414af6876cf3c3579ccaeb77de8d
Torgian11 <145939532+Torgian11@users.noreply.github.com>
2024-08-16 21:05:29 -0400

    Create slices.rs
    Slices and learning to use slices in Rust

---
+++ slices.rs
@@ -0,0 +1,19 @@
+fn main() {
+   let my_string = String::from("hello world");
+
+   let word = first_word(&my_string);
+   println!("the first word is: {word}");
+}
+
+fn first_word(s: &str) -> &str {
+   let bytes = s.as_bytes();
+
+   for (i, &item) in bytes.iter().enumerate() {
+      if item == b' ' {
+         return &s[0..i];
+      }
+   }
+
+   &s[..]
+}
+
