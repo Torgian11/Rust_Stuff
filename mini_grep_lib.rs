6f6d95c66b6bce50bf391e5a4c20342677b3a9c1
Torgian11 <145939532+Torgian11@users.noreply.github.com>
2024-09-22 19:38:22 -0400

    Update mini_grep_lib.rs

--- mini_grep_lib.rs
+++ mini_grep_lib.rs
@@ -115,3 +115,22 @@ Trust me.";
         assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
 }
+
+
+// USED IN main.rs FILE!
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
